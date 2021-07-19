f504334084745278d35052cbd13a7b5709de5880
xFutuxe <65718499+xFutuxe@users.noreply.github.com>
2021-07-19 10:09:02 +0100

    Updated redirect port from 3000 to 8888

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -102,7 +102,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/#' +
+        res.redirect('http://localhost:8888/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
